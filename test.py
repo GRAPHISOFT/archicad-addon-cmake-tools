1df0ad87bf547e92b98fe20c9a5d1e5331918855
mKrisztianGraphi <143725555+mKrisztianGraphi@users.noreply.github.com>
2023-11-03 12:57:44 +0200

    Fix code quality and missing string formating

--- test.py
+++ test.py
@@ -10,21 +10,74 @@ import urllib.parse
 import urllib.request
 import zipfile
 
-def DownloadAndUnzip (url, dest, platform):
+def ParseArguments():
+    parser = argparse.ArgumentParser ()
+    parser.add_argument ('--acVersion', dest = 'acVersion', type = str, required = True, help = 'Archicad version number list. Ex: "26 27"')
+    parser.add_argument ('--language', dest = 'language', type = str, required = False, help = 'Add-On language code list. Ex: "INT GER". Specify "ALL" for all languages in the configfile.' )
+    parser.add_argument ('--devKitPath', dest = 'devKitPath', type = str, required = False, help = 'Path to local APIDevKit')
+    parser.add_argument ('--release', dest = 'release', required = False, action='store_true', help = 'Build in localized Release mode.')
+    parser.add_argument ('--package', dest = 'package', required = False, action='store_true', help = 'Create zip archive.')
+    return parser.parse_args ()
+
+def ChangePermission(devKitFolder):
+    permissionParams = []
+    permissionParams.append ('chmod')
+    permissionParams.append ('+x')
+    permissionParams.append (str(devKitFolder / 'Support' / 'Tools' / 'OSX' / 'ResConv'))
+    permissionResult = subprocess.call (permissionParams)
+    return permissionResult == 0
+
+def PrepareDirectories(rootFolder, buildFolder, packageRootFolder, devKitFolderList, args, configData, platformName):
+    acVersionList = args.acVersion.split()
+
+    os.chdir (rootFolder)
+
+    if not buildFolder.exists():
+        buildFolder.mkdir(parents=True)
+
+    if args.package:
+        if (packageRootFolder).exists():
+            shutil.rmtree (packageRootFolder)
+
+    # Set APIDevKit directory if local is used, else create new directories
+    if args.devKitPath is not None:
+        devKitFolderList[acVersionList[0]] = pathlib.Path(args.devKitPath)
+    else:
+        # For every ACVersion
+        # Check if APIDevKitLink is provided
+        # Create directory for APIDevKit
+        # Download APIDevKit
+        for version in acVersionList:
+            if version in configData['devKitLinks']:
+
+                devKitFolder = rootFolder / f'APIDevKit-{version}'
+                if not devKitFolder.exists():
+                    devKitFolder.mkdir()
+
+                devKitFolderList[version] = devKitFolder
+                DownloadAndUnzip(configData['devKitLinks'][version], devKitFolder, platformName)
+
+            else:
+                print('APIDevKit download link not provided')
+                return 1
+        
+    return 0
+
+def DownloadAndUnzip (url, dest, platformName):
     # https://github.com/GRAPHISOFT/archicad-api-devkit/releases/download/<tag>/ <filename>
     # https://github.com/GRAPHISOFT/archicad-api-devkit/releases/latest/download/ <filename>   - not used, as there isn't a single latest version
 
     version = url.split('/')[-1]
-    fileName = f'API.Development.Kit.{platform}.{version}.zip'
-    url += '/' + fileName
+    fileName = f'API.Development.Kit.{platformName}.{version}.zip'
+    url = f'{url}/{fileName}'
     filePath = pathlib.Path(dest, fileName)
     if filePath.exists():
         return
- 
-    print ('Downloading ' + fileName)
+
+    print (f'Downloading {fileName}')
     urllib.request.urlretrieve (url, filePath)
- 
-    print ('Unzipping ' + fileName)
+
+    print (f'Unzipping {fileName}')
     if platform.system () == 'Windows':
         with zipfile.ZipFile (filePath, 'r') as zip:
             zip.extractall (dest)
@@ -49,15 +102,15 @@ def BuildAddOn (rootFolder, buildFolder, devKitFolder, addOnName, platformName, 
     elif platformName == 'MAC':
         projGenParams.extend (['-G', 'Xcode'])
 
-    projGenParams.append ('-DAC_API_DEVKIT_DIR=' + str(devKitFolder / 'Support'))
-    projGenParams.append ('-DCMAKE_BUILD_TYPE=' + configuration)
+    projGenParams.append (f'-DAC_API_DEVKIT_DIR={str(devKitFolder / "Support")}')
+    projGenParams.append (f'-DCMAKE_BUILD_TYPE={configuration}')
 
     if languageCode is not None:
-        projGenParams.append ('-DAC_ADDON_LANGUAGE=' + languageCode)
+        projGenParams.append (f'-DAC_ADDON_LANGUAGE={languageCode}')
 
     if optionalParams is not None:
         for key in optionalParams:
-            projGenParams.append ('-D' + key + '=' + optionalParams[key])
+            projGenParams.append (f'-D{key}={optionalParams[key]}')
 
     projGenParams.append (str(rootFolder))
 
@@ -65,7 +118,7 @@ def BuildAddOn (rootFolder, buildFolder, devKitFolder, addOnName, platformName, 
     if projGenResult != 0:
         print ('Failed to generate project')
         return 1
-    
+
     # Add params to build AddOn
     buildParams = []
     buildParams.append ('cmake')
@@ -76,7 +129,7 @@ def BuildAddOn (rootFolder, buildFolder, devKitFolder, addOnName, platformName, 
     if buildResult != 0:
         print ('Failed to build project')
         return 1
-    
+
     return 0
 
 def CopyResultToPackage(packageRootFolder, buildFolder, addOnName, platformName, configuration, languageCode=None, isRelease=False):
@@ -93,43 +146,37 @@ def CopyResultToPackage(packageRootFolder, buildFolder, addOnName, platformName,
 
     fileName = addOnName
     if not isRelease:
-        fileName = fileName + '_' + configuration
+        fileName = f'{fileName}_{configuration}'
 
     if platformName == 'WIN':
         shutil.copy (
-            sourceFolder / (addOnName + '.apx'),
-            packageFolder / (fileName + '.apx')
+            sourceFolder / f'{addOnName}.apx',
+            packageFolder / f'{fileName}.apx',
         )
         if configuration == 'Debug':
             shutil.copy (
-                sourceFolder / (addOnName + '.pdb'),
-                packageFolder / (fileName + '.pdb')
-            )
+            sourceFolder / f'{addOnName}.pdb',
+            packageFolder / f'{fileName}.pdb',
+        )
 
     elif platformName == 'MAC':
         subprocess.call ([
             'cp', '-r',
-            sourceFolder / (addOnName + '.bundle'),
-            packageFolder / (fileName + '.bundle')
+            sourceFolder / f'{addOnName}.bundle',
+            packageFolder / f'{fileName}.bundle'
         ])
-    
+
 
 
 def Main():
-    parser = argparse.ArgumentParser ()
-    parser.add_argument ('--acVersion', dest = 'acVersion', type = str, required = True, help = 'Archicad version number list. Ex: "26 27"')
-    parser.add_argument ('--language', dest = 'language', type = str, required = False, help = 'Add-On language code list. Ex: "INT GER". Specify "ALL" for all languages in the configfile.' )
-    parser.add_argument ('--devKitPath', dest = 'devKitPath', type = str, required = False, help = 'Path to local APIDevKit')
-    parser.add_argument ('--release', dest = 'release', required = False, action='store_true', help = 'Build in localized Release mode.')
-    parser.add_argument ('--package', dest = 'package', required = False, action='store_true', help = 'Create zip archive.')
-    args = parser.parse_args ()
+    args = ParseArguments()
 
     if args.language is None and args.release:
         print('Must specify AddOn language on release version')
         return 1
 
     acVersionList = args.acVersion.split()
-        
+
     if args.devKitPath is not None and len(acVersionList) != 1:
         print('Only one Archicad version supported with local APIDevKit option')
         return 1
@@ -163,39 +210,12 @@ def Main():
 
     # Create directory for Build and Package
     rootFolder = pathlib.Path(__file__).parent.absolute()
-    os.chdir (rootFolder)
-
     buildFolder = rootFolder / 'Build'
-    if not buildFolder.exists():
-        buildFolder.mkdir(parents=True)
-
     packageRootFolder = buildFolder / 'Package' / addOnName
-    if args.package:
-        if (packageRootFolder).exists():
-            shutil.rmtree (packageRootFolder)
-
-    # Set APIDevKit directory if local is used, else create new directories
     devKitFolderList = {}
-    if args.devKitPath is not None:
-        devKitFolderList[acVersionList[0]] = pathlib.Path(args.devKitPath)
-    else:
-        # For every ACVersion
-        # Check if APIDevKitLink is provided
-        # Create directory for APIDevKit
-        # Download APIDevKit
-        for version in acVersionList:
-            if version in configData['devKitLinks']:
 
-                devKitFolder = rootFolder / f'APIDevKit-{version}'
-                if not devKitFolder.exists():
-                    devKitFolder.mkdir()
-
-                devKitFolderList[version] = devKitFolder
-                DownloadAndUnzip(configData['devKitLinks'][version], devKitFolder, platformName)
-
-            else:
-                print('APIDevKit download link not provided')
-                return 1
+    if PrepareDirectories(rootFolder, buildFolder, packageRootFolder, devKitFolderList, args, configData, platformName) != 0:
+        return 1
 
     # At this point, devKitFolderList dictionary has all provided ACVersions as keys
     # For every ACVersion
@@ -207,15 +227,10 @@ def Main():
         devKitFolder = devKitFolderList[version]
 
         if platformName == 'MAC':
-            permissionParams = []
-            permissionParams.append ('chmod')
-            permissionParams.append ('+x')
-            permissionParams.append (str(devKitFolder / 'Support' / 'Tools' / 'OSX' / 'ResConv'))
-            permissionResult = subprocess.call (permissionParams)
-            if permissionResult != 0:
+            if not ChangePermission(devKitFolder):
                 print ('Failed to grant permission')
                 return 1
-            
+
         if args.release is True:
             for languageCode in languageList:
                 if BuildAddOn(rootFolder, buildFolder, devKitFolder, addOnName, platformName, 'RelWithDebInfo', optionalParams, languageCode) != 0:
@@ -237,11 +252,12 @@ def Main():
         release = 'Release_Localized' if args.release else 'Daily'
         subprocess.call ([
             '7z', 'a',
-            str(packageRootFolder.parent / (addOnName + '_' + release + '_' + platformName + '.zip')),
+            str(packageRootFolder.parent / f'{addOnName}_{release}_{platformName}.zip'),
             str(packageRootFolder / '*')
         ])
-    
+
     print ('Build Successful')
     return 0
 
-sys.exit (Main ())
+if __name__ == "__main__":
+    Main ()
