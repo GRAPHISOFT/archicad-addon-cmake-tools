4ecf87dfa58529e1afd8f84d8363ca82a4202200
abelcsik <168168310+abelcsik@users.noreply.github.com>
2024-10-09 14:15:42 +0200

    Update CMakeCommon.cmake
    Use force include instead of precompiled headers.
    
    Co-authored-by: vhorvath-gs <107682941+vhorvath-gs@users.noreply.github.com>

--- CMakeCommon.cmake
+++ CMakeCommon.cmake
@@ -222,11 +222,23 @@ function (GenerateAddOnProject target acVersion devKitDir addOnName addOnSources
     target_include_directories (${target} PUBLIC ${addOnSourcesFolder})
 
     # use GSRoot custom allocators consistently in the Add-On
-    target_precompile_headers(${target} 
-        PRIVATE 
-        ${devKitDir}/Modules/GSRoot/GSNew.hpp 
-        ${devKitDir}/Modules/GSRoot/GSMalloc.hpp
+    get_filename_component(new_hpp "${devKitDir}/Modules/GSRoot/GSNew.hpp" REALPATH)
+    get_filename_component(malloc_hpp "${devKitDir}/Modules/GSRoot/GSMalloc.hpp" REALPATH)
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+        target_compile_options(
+            "${target}" PRIVATE
+            "SHELL:/FI \"${new_hpp}\""
+            "SHELL:/FI \"${malloc_hpp}\""
         )
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang\$")
+        target_compile_options(
+            "${target}" PRIVATE
+            "SHELL:-include \"${new_hpp}\""
+            "SHELL:-include \"${malloc_hpp}\""
+        )
+    else()
+        message(FATAL_ERROR "Unknown compiler ID. Please open an issue at https://github.com/GRAPHISOFT/archicad-addon-cmake-tools")
+    endif()
 
     LinkGSLibrariesToProject (${target} ${acVersion} ${devKitDir})
 
